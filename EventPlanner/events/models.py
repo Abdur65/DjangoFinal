d663e554a6fe59a8f799fef75bcb2bf903a8c673
Abdur65 <abdurbc@gmail.com>
2024-12-20 15:16:55 +0600

    first commit

---
+++ EventPlanner/events/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=200)
+    date_time = models.DateTimeField()
+    
